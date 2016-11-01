25ad5d24240ac721b0cb2e38dc45fa63f7513a9c
Alexandre Insua <alexandreinsua@gmail.com>
2016-11-01 11:24:33 +0100

    Aplicacion

---
+++ src/probas/exercicio1_olamundo.java
@@ -0,0 +1,17 @@
+package probas;
+
+public class exercicio1_olamundo {
+	/* programa olamundo */
+	
+	public static void main (String[] args){
+		
+		/* Todos os programas levan un método principal
+		 * public: é un modificador que implica que é accesible
+		 * static: implica que é de clase, non de obxecto
+		 * void: implica que non devolve nada
+		 * o único que fai este programa é mostrar a cadea "Ola, Mundo" por pantalla */
+		
+		System.out.println("Ola, Mundo.");
+	
+	}	
+}
